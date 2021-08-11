21be0879c1be3c50a69031ae6244543e1d2def9b
laripaim <larissa.paim@gmail.com>
2021-08-10 22:24:15 -0300

    Brincadeira de criança

---
+++ esconde-esconde.js
@@ -0,0 +1,9 @@
+function solucao(numero) {
+    for (let i = 21; numero >= i; i++) {
+        console.log(i)
+    } if (numero <= 20) {
+        console.log("VOCE CONSEGUE, MANINHO")
+    }
+}
+
+solucao(34)
