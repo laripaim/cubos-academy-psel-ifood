36de993b798b42f0f1a4dea087ceef5580773c66
laripaim <larissa.paim@gmail.com>
2021-08-10 22:09:40 -0300

    Combinação de cores

---
+++ cores.js
@@ -0,0 +1,13 @@
+function solucao(cor1,cor2){
+    if((cor1 === "azul" && cor2 === "amarelo") || (cor1 === "amarelo" && cor2 === "azul")) {
+    console.log("verde");
+    } else if((cor1 === "azul" && cor2 === "vermelho") || (cor1 === "vermelho" && cor2 === "azul")) {
+    console.log("roxo");
+    } else if((cor1 === "vermelho" && cor2 === "amarelo") || (cor1 === "amarelo" && cor2 === "vermelho")){
+    console.log("laranja");
+    } else if(cor1 === cor2) {
+    console.log(cor1)
+    }
+  }
+
+solucao("vermelho", "amarelo")
